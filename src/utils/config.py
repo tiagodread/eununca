8d7c33272d90a072799a614f43488d96699d4158
Tiago Góes <tiago.goes2009@gmail.com>
2019-02-14 23:34:16 -0200

    Change DB credentials

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,10 +1,10 @@
 class Config(object):
     DEBUG = True
-    DB_USER = 'postgres'
-    DB_PASS = 'admin'
+    DB_USER = 'ejrmoqqsvrdlsb'
+    DB_PASS = '7e673d42dc60e1d06649d1bf8858a9adfce2324023c065983618544dd80b52cd'
     DB_PORT = '5432'
-    DB_NAME = 'eununca'
-    DB_HOST = 'localhost'
+    DB_NAME = 'd2r6n717ei51ka'
+    DB_HOST = 'ec2-54-225-237-84.compute-1.amazonaws.com'
     SQLALCHEMY_DATABASE_URI = 'postgresql://%s:%s@%s:%s/%s' % (DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     BASIC_AUTH_USERNAME = 'tiago.goes2009@gmail.com'
